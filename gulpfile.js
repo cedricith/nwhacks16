2159cff63da880d5b9f2245eec29f9823c9f838b
Cedric Ith <cedric.ith1@gmail.com>
2016-02-28 01:19:22 -0800

    changes structure

--- gulpfile.js
+++ gulpfile.js
@@ -6,16 +6,16 @@ gulp.task('default', ['watch']);
 gulp.task('build', ['build:html', 'build:js']);
 
 gulp.task('build:js', function() {
-	return gulp.src('src/*.js')
+	return gulp.src('js/*.js')
 		.pipe(browserify({}))
 		.pipe(gulp.dest('build'));
 });
 
 gulp.task('build:html', function() {
-	return gulp.src('src/*.html')
+	return gulp.src('*.html')
 		.pipe(gulp.dest('build'));
 });
 
 gulp.task('watch', ['build'], function() {
-	gulp.watch('src/*', ['build']);
+	gulp.watch('js/*', ['build']);
 });
